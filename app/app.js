953b59ce05d95e665bc0f1284118d154b8588dd0
niksh-19 <nikita.sharma@edu.sait.ca>
2024-12-05 09:20:19 -0700

    Update

--- app/app.js
+++ app/app.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react';
-import './App.css';
 import Sidebar from './components/sidebar';
 import Weather from './components/weather';
 
 function App() {
-  const [activePage, setActivePage] = useState('forecast'); // Default to forecast page
+  const [activePage, setActivePage] = useState('forecast');
 
-  const handlePageChange = (page) => {
-    setActivePage(page);
-  };
+  const handlePageChange = (page) => setActivePage(page);
 
   return (
-    <div className="app-container">
+    <div className="flex h-screen font-sans">
       {/* Sidebar */}
       <Sidebar onPageChange={handlePageChange} />
 
       {/* Main content */}
-      <div className="main-content">
+      <div className="flex-1 bg-green-500 text-white p-6 flex justify-center items-center">
         {activePage === 'forecast' && <Weather />}
+        {activePage === 'news' && <div>News content will be added here!</div>}
       </div>
     </div>
   );
