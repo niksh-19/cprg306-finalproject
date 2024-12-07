330945a16dfee1fdfbb0d67123d4158452aff7a5
Mahroh-Afzal <152742086+Mahroh-Afzal@users.noreply.github.com>
2024-12-07 14:27:40 -0800

    firebase
    updated firebase

--- app/app.js
+++ app/app.js
@@ -3,16 +3,33 @@ import './app.css';
 import Sidebar from './components/sidebar';
 import Weather from './components/weather';
 import Contact from "./components/contact";
-
+import LogIn from './logIn';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 function App() {
+ console.log("here")
   const [activePage, setActivePage] = useState('forecast'); // Default to forecast page
+  const [isAuthenticated, setisAuthenticated]=useState(false);
 
   const handlePageChange = (page) => {
     setActivePage(page);
   };
 
+  const auth = getAuth();
+  
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setisAuthenticated(true)
+     
+    } else {
+      setisAuthenticated(false)
+    }
+  });
+
   return (
     <div className="app-container">
+      {isAuthenticated&&(
+        <>
+
       {/* Sidebar */}
       <Sidebar onPageChange={handlePageChange} />
 
@@ -20,8 +37,19 @@ function App() {
       <div className="main-content">
         {activePage === 'forecast' && <Weather />}
       </div>
-    </div>
-  );
-}
+      </>
+)}
+    {!isAuthenticated&&(
+  <>
+<LogIn>
+
+</LogIn>
+  </>
+    )
+  }
+      </div>
+
+    );
+  }
 
 export default App;
